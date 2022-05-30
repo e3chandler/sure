40cdfbeac2696952dfc615368d5af703e270cafc
e3chandler <ryann.chandler@vitalsource.com>
2022-05-29 23:43:03 -0500

    adds creating new policy holder

--- src/backend/api.ts
+++ src/backend/api.ts
@@ -1,8 +1,22 @@
 const BASE_URL= 'https://fe-interview-technical-challenge-api-git-main-sure.vercel.app'
 
-export const getJSON = async (route: string) => {
-  const response = await fetch(`${BASE_URL}${route}`)
+export const getJSON = async <Type>(route: string) : Promise<Type> => {
+  return await handleResponse(await fetch(getUrl(route)))
+}
 
+export const postJSON = async <Type>(route: string, body: Object) : Promise<Type> => {
+  let params: RequestInit = {
+    method: 'POST',
+    body: JSON.stringify(body)
+  }
+  return await handleResponse(
+    await fetch(getUrl(route), params)
+  )
+}
+
+const getUrl = (route: string): string => `${BASE_URL}${route}`
+
+const handleResponse = async (response: Response) => {
   if (!response.ok) {
     throw new Error('Network response was not ok')
   }
--- src/components/PolicyHoldersView/PolicyHoldersView.tsx
+++ src/components/PolicyHoldersView/PolicyHoldersView.tsx
@@ -1,46 +1,78 @@
-import { getJSON } from '../../backend/api' 
+import { getJSON, postJSON } from '../../backend/api' 
 import InfoTable from '../InfoTable'
 import routes from '../../constants/backend'
-import { useQuery } from 'react-query'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { Button, Box } from '@mui/material';
+import { PolicyHolder, PolicyHolderResponse } from './types'
 
-type PolicyHolder = {
-  name: string,
-  age: number,
+const getNewPolicyHolder = () : PolicyHolder => ({
+  name: 'Ryann Chandler',
+  age: 36,
   address: {
-    line1: string,
-    line2: string,
-    city: string,
-    state: string,
-    postalCode: string
+    line1: '123 ABC Street',
+    line2: 'Suite 1',
+    city: 'Nashville',
+    state: 'Tn',
+    postalCode: '37013'
   },
-  phoneNumber: string,
-  isPrimary: boolean
-};
+  phoneNumber: '615-867-5309',
+  isPrimary: false
+})
 
 const PolicyHoldersView = () => {
-  const { isLoading, error, data } = useQuery('policyHolders', () => getJSON(routes.policyHolders));
+  const queryClient = useQueryClient();
+  const query = useQuery<PolicyHolderResponse>('policyHolders', () => getJSON<PolicyHolderResponse>(routes.policyHolders));
+  const mutation = useMutation(
+    (newPolicyHolder: PolicyHolder) => postJSON<PolicyHolderResponse>(routes.policyHolders, newPolicyHolder),
+    {
+      onSuccess: (data) => {
+        queryClient.setQueryData('policyHolders', data)
+      }
+    })
 
-  if (isLoading) return <div>...Loading</div>;
-  if (error) return <div>...error</div>;
+  if (query.isLoading || mutation.isLoading) return <div>...Loading</div>;
+  if (query.error || mutation.isError) return <div>...error</div>;
 
-  const { policyHolders } = data
-  const rows = policyHolders.flatMap((policyHolder: PolicyHolder) => ([
+  const { policyHolders } = queryClient.getQueryData<PolicyHolderResponse>('policyHolders') as PolicyHolderResponse;
+  
+  const tables = policyHolders.map((policyHolder: PolicyHolder) => ([
     {key: 'Name', value: policyHolder.name},
     {key: 'Age', value: policyHolder.age},
-    {key: 'Address', value: `${policyHolder.address.line1} ${policyHolder.address.line2}, ${policyHolder.address.city} ${policyHolder.address.state} ${policyHolder.address.postalCode}`},
+    {key: 'Address', value: `${policyHolder.address.line1} ${policyHolder.address.line2}, ${policyHolder.address.city} ${policyHolder.address.state}, ${policyHolder.address.postalCode}`},
     {key: 'Phone Number', value: policyHolder.phoneNumber},
     {key: 'Is Primary', value: policyHolder.isPrimary ? 'Yes' : 'No'}
   ]));
 
-  return <InfoTable header="Policy Holders" rows={rows} />
+  return (
+    <Box
+      sx={{
+        justifyItems: 'center',
+        display: 'grid'
+      }}
+    >
+      <Box
+        sx={{
+          display: 'grid',
+          gap: '40px'
+        }}
+      >
+        {tables.map((rows, idx) => <InfoTable key={`${idx}_table`} header={`Policy Holder ${idx+1}`} rows={rows} /> )}
+      </Box>
+      
+      <Button 
+        sx={{
+          marginTop: '20px'
+        }}
+        onClick={() => mutation.mutate(getNewPolicyHolder())}
+        variant="contained"
+        color="primary"
+        size="large"
+        disabled={tables.length > 1}
+      >
+        Add a policyholder
+      </Button>
+    </Box>
+  )
 }
 
 export default PolicyHoldersView
-
-
-
-// Name
-// Age
-// Address
-// Phone number
-// Primary policyholder?
