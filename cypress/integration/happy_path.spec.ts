4bddd39de6cbae8ed03e1a1a567aa8c59bc6918f
e3chandler <ryann.chandler@vitalsource.com>
2022-05-30 23:36:17 -0500

    remove dup file for routes. fix tests.

--- cypress/integration/happy_path.spec.ts
+++ cypress/integration/happy_path.spec.ts
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 import { BASE_URL } from '../../src/backend/api';
-import { routes } from '../../src/backend/routes'
+import routes from '../../src/constants/backend'
 
 describe('happy path', () => {
   it('runs happy path successfully', () => {
@@ -16,9 +16,9 @@ describe('happy path', () => {
      * - Assert that data from the network is displayed
      */
     cy.fixture('example').then(data => {
-      cy.intercept('GET', `${BASE_URL}${routes.policyHolders.path}`, data).as('getRequest')
+      cy.intercept('GET', `${BASE_URL}${routes.policyHolders}`, data).as('getRequest')
       cy.getTestEl('policyholders_link').click()
-      cy.get('@getRequest').its('response.statusCode').should('eq', 200)
+      cy.get('@getRequest').then(response => response).its('response.statusCode').should('eq', 200)
       cy.getTestEl('Policy Holder 1').should('be.visible')
       cy.contains('Ryann Chandler').should('be.visible')
       cy.contains('123 ABC Street Unit 4, Nashville Tn, 37214').should('be.visible')
--- src/backend/routes.ts
+++
@@ -1,5 +0,0 @@
-export const routes = {
-  policyHolders: {
-    path: '/api/policyholders'
-  }
-}
